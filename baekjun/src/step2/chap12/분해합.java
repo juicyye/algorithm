f5b3737f621e77416425f8a51e9b867407bc3f27
param <spsummerdoc@gmail.com>
2024-10-08 23:50:22 +0900

    brutoforce

--- baekjun/src/step2/chap12/분해합.java
+++ baekjun/src/step2/chap12/분해합.java
@@ -3,5 +3,29 @@ package step2.chap12;
 import java.util.Scanner;
 
 public class 분해합 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = 0;
+
+        for (int i = n - String.valueOf(n).length() * 9; i < n; i++) {
+            if(i<0) continue;
+            if (sum(i) == n) {
+                result = i;
+                break;
+            }
+        }
+        System.out.println(result);
+
+    }
+
+    public static int sum(int number) {
+        int sum = number;
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
 
 }
