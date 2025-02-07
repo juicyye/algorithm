9ac61eecae2cbaf0d2b3ca9ac0f69b6fd64ad521
param <spsummerdoc@gmail.com>
2025-02-07 23:27:11 +0900

    linked list

---
+++ javaInterviewWithKotlin/src/main/kotlin/linkedlist/재귀구조로뒤집기.kt
@@ -0,0 +1,15 @@
+package hello.linkedlist
+
+/*
+fun reverseList(head: ListNode?): ListNode? {
+    var prev: ListNode? = null
+    var node = head
+
+    while(node != null){
+        val next = node.next
+        node.next = prev
+        prev = node
+        node = next
+    }
+    return prev
+}*/
---
+++ javaInterviewWithKotlin/src/main/kotlin/linkedlist/페어의노드스왑.kt
@@ -0,0 +1,14 @@
+package hello.linkedlist
+
+/*
+fun swapPairs(head: ListNode?): ListNode? {
+    if (head?.next != null){
+        val p = head.next
+
+        head.next = swapPairs(head.next.next)
+
+        p.next = head
+        return p
+    }
+    return head
+}*/
